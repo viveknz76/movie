ee452c89d0a9340113c7bfe135fd8b9cc796eecc
Vivek Sharma <viveknz76@gmail.com>
2022-06-26 10:49:30 +1200

    Axios initial implementation
    Initial axios implementation to get the genre data

--- src/genres/IndexGenres.tsx
+++ src/genres/IndexGenres.tsx
@@ -1,6 +1,17 @@
+import axios, { AxiosResponse } from 'axios';
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { genreDTO } from './genres.model';
 
 export default function IndexGenres() {
+  useEffect(() => {
+    axios
+      .get('https://localhost:7006/api/genres')
+      .then((response: AxiosResponse<genreDTO[]>) => {
+        console.log(response.data);
+      });
+  }, []);
+
   return (
     <>
       <h3>Genres</h3>
