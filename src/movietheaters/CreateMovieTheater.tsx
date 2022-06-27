d2704a25349aad28b3c9fccc9fe5e6fcb08aab45
Vivek Sharma <viveknz76@gmail.com>
2022-06-27 13:45:24 +1200

    Create a movie theatre
    Added creating a movie theatre functionality

--- src/movietheaters/CreateMovieTheater.tsx
+++ src/movietheaters/CreateMovieTheater.tsx
@@ -1,14 +1,33 @@
+import axios from 'axios';
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { urlMovieTheaters } from '../endpoints';
+import DisplayErrors from '../utils/DisplayErrors';
+import { movieTheaterCreationDTO } from './movieTheater.model';
 import MovieTheaterForm from './MovieTheaterForm';
 
 export default function CreateMovieTheater() {
+  const [errors, setErrors] = useState<string[]>([]);
+  const history = useHistory();
+
+  async function create(movieTheater: movieTheaterCreationDTO) {
+    try {
+      await axios.post(urlMovieTheaters, movieTheater);
+      history.push('/movietheaters');
+    } catch (error) {
+      if (error && error.response) {
+        setErrors(error.response.data);
+      }
+    }
+  }
   return (
     <>
       <h3>Create movie theater</h3>
+      <DisplayErrors errors={errors} />
       <MovieTheaterForm
         model={{ name: '' }}
         onSubmit={async (values) => {
-          await new Promise((r) => setTimeout(r, 1));
-          console.log(values);
+          await create(values);
         }}
       />
     </>
